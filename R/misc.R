afdb6dea2635860a54ea14e33c1f451d2b422007
Garrick Sherman <garrick.sherman@nih.gov>
2026-08-07 16:00:44 -0400

    added anonymize function

--- R/misc.R
+++ R/misc.R
@@ -5,3 +5,14 @@ print_all = function(df) {
   df %>%
     print(n = nrow(.))
 }
+
+#' Anonymize a tibble/data frame column with one-way hashing
+#'
+#' @export
+anonymize = function(df, col, char_length = 10) {
+  df %>%
+    dplyr::mutate(dplyr::across({{ col }},
+                  ~stringr::str_sub(
+                    digest::getVDigest('sha1')({{ col }}),
+                    end = char_length)))
+}
